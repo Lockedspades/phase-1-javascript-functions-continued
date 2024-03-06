a4fff2a1fa2970900c09bde765d6e245a721598a
Alex Sanchez <lockedasanchez@gmail.com>
2024-03-06 16:31:47 -0500

    Complete Lab(3/6)

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(stuff = 'go to the office'){
+    return `This Monday, I will ${stuff}.`;
+}
+function wrapAdjective(parameter= '*'){
+    return function(adjective='special') {
+        return `You are ${parameter}${adjective}${parameter}!`;
+          };
+        }
+    
